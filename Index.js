7100f36a885e27253344c7728e1b765ed49595cb
Yaniv Shmulevich <yaniv.shmul@live.com>
2018-07-15 01:22:40 +0300

    First commit from VS Code

---
+++ Index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const port = process.env.port || 3000;
+
+
+const handler = (req,res) => {
+    console.log("Server received request. ");
+    res.end('Hello Azure from Local Git!!!');
+}
+
+const server = http.createServer(handler);
+
+server.listen(port, err => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(`Server listening on port: ${port}`);
+    }
+}
+);
