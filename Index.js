94a1cb702ee4a9b652c3318ad6c2c7618ad05fe9
Yaniv Shmulevich <yansh@YANSH-LAP.cala.corp>
2018-07-16 23:46:32 +0300

    new born 33

--- Index.js
+++ Index.js
@@ -1,17 +1,9 @@
 const http = require("http");
-const figlet = require("figlet");
 const port = process.env.port || 3000; 
 
 const handler = (req,res) => {
     console.log("Server received request. ");
-    figlet('Hello World', (err,data) => {
-        if(err) {
-            console.log('Something went wrong ...');
-            console.log(err);
-            return;
-        }
-        res.end(data);
-    });
+    res.end(process.env.CONNECTION_STRING);
 }
 
 const server = http.createServer(handler);
